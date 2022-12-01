39db5c0bb48ed6bd987938ca4ba72647586b6f70
Patrick Crumley <patrick.crumley@gmail.com>
2022-11-30 21:19:47 -0800

    Revert "finish day 1;"
    This reverts commit f19c65ebb821d140caf72d0657f5ddd903b667cb.

--- day_1/target/debug/deps/day_1-892819deb7f6ec09.d
+++
@@ -1,6 +0,0 @@
-/Users/patrick/Code/advent_of_code_2022/day_1/target/debug/deps/day_1-892819deb7f6ec09.rmeta: src/main.rs src/../input.txt
-
-/Users/patrick/Code/advent_of_code_2022/day_1/target/debug/deps/day_1-892819deb7f6ec09.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
