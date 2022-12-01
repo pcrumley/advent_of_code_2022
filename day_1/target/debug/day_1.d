a1676e2050f418ce9fc26a6b7bf9944137483c48
Patrick Crumley <patrick.crumley@gmail.com>
2022-11-30 21:21:18 -0800

    delete target files

--- day_1/target/debug/day_1.d
+++
@@ -1,1 +0,0 @@
-/Users/patrick/Code/advent_of_code_2022/day_1/target/debug/day_1: /Users/patrick/Code/advent_of_code_2022/day_1/input.txt /Users/patrick/Code/advent_of_code_2022/day_1/src/main.rs
--- day_1/target/debug/deps/day_1-ba647b568f8a15ae.d
+++
@@ -1,6 +0,0 @@
-/Users/patrick/Code/advent_of_code_2022/day_1/target/debug/deps/day_1-ba647b568f8a15ae: src/main.rs src/../input.txt
-
-/Users/patrick/Code/advent_of_code_2022/day_1/target/debug/deps/day_1-ba647b568f8a15ae.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
